1ac463e21384f15b570dbcca80c8347a431b3123
Cemah Tudae-Torboh <cemah.tudae-torboh@accenture.com>
2015-06-09 22:57:06 -0400

    6/9 committ 1

--- java/com/entities/employee.java
+++ java/com/entities/employee.java
@@ -1,5 +1,87 @@
 package com.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
 public class employee extends personnel {
+
+    private boolean isManager;
+
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    @Override
+    public Long getId() {
+
+        return this.id;
+
+    }
+
+    @Column(name = "firstName")
+    @Override
+    public String getFirstName() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "lastName")
+    @Override
+    public String getLastName() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "startDate")
+    @Override
+    public String getStartDate() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "endDate")
+    @Override
+    public String getEndDate() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "email")
+    @Override
+    public String getEmail() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "login")
+    @Override
+    public String getLogin() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Column(name = "password")
+    @Override
+    public String getPassword() {
+
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setMangerStatus() {
+
+    }
 
 }
--- java/com/entities/personnel.java
+++ java/com/entities/personnel.java
@@ -1,16 +1,84 @@
 package com.entities;
 
-public class personnel {
-private Long id;
-private String firstName;
-private String lastName;
-private String startDate;
-private String endDate;
-private String email;
+public abstract class personnel
+    implements Comparable<personnel> {
 
-    private String login;
+    protected Long id;
 
-    private String password;
-    // / some comment
+    protected String firstName;
+
+    protected String lastName;
+
+    protected String startDate;
+
+    protected String endDate;
+
+    protected String email;
+
+    protected String login;
+
+    protected String password;
+
+
+    @Override
+    public int hashCode() throws NullPointerException {
+
+        int hash = 8;
+        hash = (int) (hash * id);
+        return hash;
+    }
+
+    public boolean equals(Object obj) throws NullPointerException {
+
+        if (obj == null) {
+            return false;
+
+        }
+
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+
+        personnel per = (personnel) obj;
+
+        if (this.id == null && per.id == null) {
+            return false;
+        } else if (this.id == null || per.id == null) {
+            return false;
+
+        }
+
+        return this.id.equals(per.id);
+    }
+
+    @Override
+    public int compareTo(personnel per) {
+
+        int cmpt = this.lastName.compareToIgnoreCase(per.lastName);
+        if (cmpt == 0) {
+            cmpt = this.firstName.compareToIgnoreCase(per.firstName);
+            if (cmpt == 0) {
+                cmpt = this.id.compareTo(per.id);
+            }
+        }
+        return cmpt;
+    }
+
+    public abstract Long getId();
+
+    public abstract String getFirstName();
+
+    public abstract String getLastName();
+
+    public abstract String getStartDate();
+
+    public abstract String getEndDate();
+
+    public abstract String getEmail();
+
+    public abstract String getLogin();
+
+    public abstract String getPassword();
+
 
 }
